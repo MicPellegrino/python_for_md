28b7a6506dadf9a70af5e352babd2e7b04b8467a
MicPellegrino <micpel@kth.se>
2021-04-22 23:04:28 +0200

    LT ti odio

--- free_energy_analysis.py
+++ free_energy_analysis.py
@@ -18,77 +18,98 @@ gamma = 5.78e-2         # Pa*m
 # Avogadro's number
 N_A = 6.02214076e23     # mol^-1
 
-xvg_res =      [ '/home/michele/python_for_md/FreeEnergyCorrugated/FE06_res/dhdl.xvg',      ]
-xvg_sol =      [ '/home/michele/python_for_md/FreeEnergyCorrugated/FE06_sol/dhdl.xvg',      ]
-gro_init_res = [ '/home/michele/python_for_md/FreeEnergyCorrugated/FE06_res/init_conf.gro', ]
-gro_fin_res =  [ '/home/michele/python_for_md/FreeEnergyCorrugated/FE06_res/confout.gro',   ]
+label = ['00', '01', '02', '03', '04', '05', '06']
+a = np.linspace(0.1, 0.7, 7)
+
+xvg_res = []
+xvg_sol = []
+gro_init_res = []
+gro_fin_res = []
+for l in label :
+    xvg_res.append('/home/michele/python_for_md/FreeEnergyCorrugate/FE'+l+'_res/dhdl.xvg')
+    xvg_sol.append('/home/michele/python_for_md/FreeEnergyCorrugate/FE'+l+'_sol/dhdl.xvg')
+    gro_init_res.append('/home/michele/python_for_md/FreeEnergyCorrugate/FE'+l+'_res/init_conf.gro')
+    gro_fin_res.append('/home/michele/python_for_md/FreeEnergyCorrugate/FE'+l+'_res/confout.gro')
 
 conf_init = md.Configuration()
 conf_init.input(gro_init_res[0])
 conf_fin = md.Configuration()
 conf_fin.input(gro_fin_res[0])
-
 Ly = (conf_init.box_yy)*1e-9
 Lx0 = (conf_init.box_xx)*1e-9
 Lx1 = (conf_fin.box_xx)*1e-9
+diff_H_sur = [ (2.0*N_A*gamma*Ly*(Lx1-Lx0))*1e-3, ]
 
-# Free-energy difference due to surface tension
-diff_H_sur = (2.0*N_A*gamma*Ly*(Lx1-Lx0))*1e-3
-print("diff_H_sur = "+str(diff_H_sur)+" kJ/mol")
+k_example = 4
+
+for k in range(1, len(label)) :
+    conf_init.input(gro_init_res[k])
+    conf_fin.input(gro_fin_res[k])
+    Lx0 = (conf_init.box_xx)*1e-9
+    Lx1 = (conf_fin.box_xx)*1e-9
+    diff_H_sur.append( (2.0*N_A*gamma*Ly*(Lx1-Lx0))*1e-3 )
 
 t, _ = read_xvg_file( xvg_res[0] )
-
 lam = np.array(t) / max(t)
-dt = lam[1]-lam[0]
-delta_lam = 2.0e-6
-
-dhdl = np.zeros( (len(xvg_res), len(t)), dtype=float )
-for k in range(len(xvg_res)) :
-    _, y = read_xvg_file( xvg_res[k] )
-    dhdl[k,:] = np.array(y)
-dhdl_avg = np.mean(dhdl, axis=0)
-delta_h = dt*np.cumsum(dhdl_avg)
-diff_H_res = np.mean(dhdl_avg)
-print("diff_H_res = "+str(diff_H_res)+" kJ/mol")
+dl = lam[1]-lam[0]
 
 fig1, (ax1, ax2) = plt.subplots(1, 2)
 
-ax1.plot(lam, dhdl_avg, 'g-', linewidth=0.25)
-ax1.plot(lam, delta_h, 'r-', linewidth=2.5, label='integral')
-ax1.plot(lam, np.zeros(len(lam)), 'k--', linewidth=2.5)
+diff_H_res = []
+for k in range(len(label)) :
+    _, dhdl_avg = read_xvg_file( xvg_res[k] )
+    delta_h = dl*np.cumsum(dhdl_avg)
+    diff_H_res.append(delta_h[-1])
+    if k == k_example :
+        ax1.plot(t, dhdl_avg, 'g-', linewidth=0.25)
+        ax1.plot(t, np.zeros(len(t)), 'k--', linewidth=2.5)
+        ax1.plot(t, delta_h, 'r-', linewidth=2.5, label='integral')
+        ax1.set_title('Dry', fontsize=35.0)
+        ax1.set_xlabel('$t$', fontsize=25.0)
+        ax1.set_ylabel(r'$<dH/d\lambda>$', fontsize=25.0)
+        ax1.legend(fontsize=25.0)
+        ax1.tick_params(axis='x', labelsize=20.0)
+        ax1.tick_params(axis='y', labelsize=20.0)
+        y_min = min(dhdl_avg)
+        y_max = max(dhdl_avg)
 
-ax1.set_title('Dry')
-ax1.set_ylabel(r'$<dH/d\lambda>$')
-ax1.set_xlabel(r'$\lambda$')
-ax1.legend()
+diff_H_sol = []
+for k in range(len(label)) :
+    _, dhdl_avg = read_xvg_file( xvg_sol[k] )
+    delta_h = dl*np.cumsum(dhdl_avg)
+    diff_H_sol.append(delta_h[-1])
+    if k == k_example :
+        ax2.plot(t, dhdl_avg, 'c-', linewidth=0.25)
+        ax2.plot(t, np.zeros(len(t)), 'k--', linewidth=2.5)
+        ax2.plot(t, delta_h, 'r-', linewidth=2.5)
+        ax2.set_title('Wet', fontsize=35.0)
+        ax2.set_xlabel('$t$', fontsize=25.0)
+        ax2.tick_params(axis='x', labelsize=20.0)
+        ax2.tick_params(axis='y', labelsize=20.0)
+        y_min = min( y_min, min(dhdl_avg))
+        y_max = max( y_max, max(dhdl_avg))
+        ax1.set_xlim([0.0, max(t)])
+        ax1.set_ylim([y_min, y_max])
+        ax2.set_xlim([0.0, max(t)])
+        ax2.set_ylim([y_min, y_max])
 
-max_y = max(dhdl_avg)
-min_y = min(dhdl_avg)
+plt.show()
 
-dhdl = np.zeros( (len(xvg_res), len(t)), dtype=float )
-for k in range(len(xvg_sol)) :
-    _, y = read_xvg_file( xvg_sol[k] )
-    dhdl[k,:] = np.array(y)
-dhdl_avg = np.mean(dhdl, axis=0)
-delta_h = dt*np.cumsum(dhdl_avg)
-diff_H_sol = np.mean(dhdl_avg)
-print("diff_H_sol = "+str(diff_H_sol)+" kJ/mol")
+diff_H_sol = np.array(diff_H_sol)
+diff_H_res = np.array(diff_H_res)
+diff_H_sur = np.array(diff_H_sur)
 
 diff_H_ws = diff_H_sol - diff_H_res - diff_H_sur
 
-print("excess WS  = "+str(diff_H_ws)+" kJ/mol")
-
-ax2.plot(lam, dhdl_avg, 'c-', linewidth=0.25)
-ax2.plot(lam, delta_h, 'r-', linewidth=2.5)
-ax2.plot(lam, np.zeros(len(lam)), 'k--', linewidth=2.5)
-
-ax2.set_title('Solvated')
-ax2.set_xlabel(r'$\lambda$')
-
-max_y = max(max_y,max(dhdl_avg))
-min_y = min(min_y,min(dhdl_avg))
-
-ax1.set_ylim(min_y, max_y)
-ax2.set_ylim(min_y, max_y)
-
+plt.plot(a, diff_H_sol, 'ko--', linewidth=1.5, markersize=8.0, label='total')
+plt.plot(a, diff_H_res, 'gs--', linewidth=1.5, markersize=8.0, label='restraints')
+plt.plot(a, diff_H_sur, 'bx--', linewidth=1.5, markersize=8.0, label='surface')
+plt.plot(a, diff_H_ws,  'rd--', linewidth=2.5, markersize=12.0, label='excess')
+plt.title('Free energy analysis', fontsize=35.0)
+plt.legend(fontsize=25.0)
+plt.xlim(0.0, 0.8)
+plt.xticks(fontsize=20.0)
+plt.yticks(fontsize=20.0)
+plt.ylabel('$H$', fontsize=25.0)
+plt.xlabel('$a$', fontsize=25.0)
 plt.show()
